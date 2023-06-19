d6a77e5b832aa9496697151862cf61e6ea64f05e
hamid <hr454837@gamil.com>
2023-06-19 20:12:15 +0330

    Update database

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,3 +1,9 @@
+CREATE TYPE "baseUserManager" AS ENUM (
+  'is_active',
+  'is_admin',
+  'psssword'
+);
+
 CREATE TABLE "subscribe" (
   "subscriber_user_id" integer,
   "subscribed_user_id" integer,
@@ -6,12 +12,12 @@ CREATE TABLE "subscribe" (
 
 CREATE TABLE "users" (
   "id" integer PRIMARY KEY,
-  "email" varchar,
-  "password" integer,
-  "created_at" timestamp DEFAULT (now()),
+  "email" varchar NOT NULL,
+  "created_at" timestamp NOT NULL DEFAULT (now()),
   "object" baseUserManager,
   "is_active" bool DEFAULT True,
-  "is_admin" bool DEFAULT False
+  "is_admin" bool DEFAULT False,
+  "password" integer NOT NULL
 );
 
 CREATE TABLE "posts" (
