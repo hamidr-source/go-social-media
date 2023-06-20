af56e382d821ece7884e5f6e8fccd1f493499198
hamid <hr454837@gamil.com>
2023-06-21 00:08:24 +0330

    Edit the database

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,34 +1,40 @@
 CREATE TABLE "subscription" (
-  "subscriber" integer,
-  "target" integer
+  "subscriber" integer NOT NULL,
+  "traget" integer NOT NULL
 );
 
 CREATE TABLE "user" (
   "id" integer UNIQUE PRIMARY KEY,
   "email" varchar(255) NOT NULL,
   "password" integer NOT NULL,
-  "created_at" timestamp DEFAULT (now()),
-  "is_active" bool DEFAULT True,
-  "is_admin" bool DEFAULT False
+  "created_at" timestampz NOT NULL DEFAULT (now()),
+  "is_active" bool NOT NULL DEFAULT True,
+  "is_admin" bool NOT NULL DEFAULT False
 );
 
 CREATE TABLE "post" (
   "slug" slugFeild(100) PRIMARY KEY NOT NULL,
   "title" varchar(100) UNIQUE,
   "content" text(1000) NOT NULL,
-  "author" integer,
-  "created_at" timestamp DEFAULT (now())
+  "author" integer NOT NULL,
+  "created_at" timestampz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "profile" (
-  "user" varchar,
-  "post_count" positiveIntrger DEFAULT 0,
-  "subscriber_count" positiveIntrger DEFAULT 0,
-  "subscription_count" positiveIntrger DEFAULT 0,
+  "user" varchar NOT NULL,
+  "post_count" integer NOT NULL DEFAULT 0,
+  "subscriber_count" integer NOT NULL DEFAULT 0,
+  "subscription_count" integer NOT NULL DEFAULT 0,
   "bio" varchar(1000)
 );
 
 COMMENT ON COLUMN "post"."content" IS 'Content of the post';
+
+COMMENT ON COLUMN "profile"."post_count" IS 'positive';
+
+COMMENT ON COLUMN "profile"."subscriber_count" IS 'positive';
+
+COMMENT ON COLUMN "profile"."subscription_count" IS 'positive';
 
 ALTER TABLE "post" ADD FOREIGN KEY ("author") REFERENCES "user" ("id") ON DELETE SET NULL;
 
@@ -47,10 +53,10 @@ ALTER TABLE "user_subscription" ADD FOREIGN KEY ("subscription_subscriber") REFE
 
 CREATE TABLE "user_subscription(1)" (
   "user_id" integer,
-  "subscription_target" integer,
-  PRIMARY KEY ("user_id", "subscription_target")
+  "subscription_traget" integer,
+  PRIMARY KEY ("user_id", "subscription_traget")
 );
 
 ALTER TABLE "user_subscription(1)" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
 
-ALTER TABLE "user_subscription(1)" ADD FOREIGN KEY ("subscription_target") REFERENCES "subscription" ("target");
+ALTER TABLE "user_subscription(1)" ADD FOREIGN KEY ("subscription_traget") REFERENCES "subscription" ("traget");
