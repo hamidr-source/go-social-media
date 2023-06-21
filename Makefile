35fe205de1e0ddaeade8523b170dfe3c1a4cb933
hamid <hr454837@gamil.com>
2023-06-21 22:00:43 +0330

    Edit sql and makefile

--- Makefile
+++ Makefile
@@ -1,10 +1,16 @@
 postgres:
-	docker run --name postgres12 -p 5432:5432 -e POSTGRES_USER=root -e POSGRES_PASSWORD=secret -d postgres
+	docker run --name postgres11 -p 5432:5432 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=secret -d postgres:12-alpine
 
 createdb:
-	docker exec -it postgres12 createdb --username=root --owner=root social_media
+	docker exec -it postgres11 createdb --username=root --owner=root go_socialmedia
 
 dropdb:
-	docker exec -it postgres12 dropdb social_media
+	docker exec -it postgres11 dropdb go_socialmedia
 
-.PHONY: postgres createdb dropdb
+migrateup:
+	migrate -path db/migration -database "postgresql://root:secret@localhost:5432/go_socialmedia?sslmode=disable" -verbose up
+
+migratedown:
+	migrate -path db/migration -database "postgresql://root:secret@localhost:5432/go_socialmedia?sslmode=disable" -verbose down
+
+.PHONY: postgres createdb dropdb migrateup migratedown
--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,34 +1,31 @@
 CREATE TABLE "subscription" (
-  "subscriber" integer NOT NULL,
-  "traget" integer NOT NULL
+  "subscriber" integer,
+  "target" integer
 );
 
 CREATE TABLE "user" (
-  "id" integer UNIQUE PRIMARY KEY,
-  "email" varchar(255) NOT NULL,
-  "password" integer NOT NULL,
-  "created_at" timestampz NOT NULL DEFAULT (now()),
-  "is_active" bool NOT NULL DEFAULT True,
-  "is_admin" bool NOT NULL DEFAULT False
-);
-
-CREATE TABLE "post" (
-  "slug" slugFeild(100) PRIMARY KEY NOT NULL,
-  "title" varchar(100) UNIQUE,
-  "content" text(1000) NOT NULL,
-  "author" integer NOT NULL,
-  "created_at" timestampz NOT NULL DEFAULT (now())
+  "id" integer PRIMARY KEY,
+  "email" varchar(255),
+  "is_admin" boolean DEFAULT False,
+  "is_active" boolean DEFAULT True,
+  "created_at" timestamptz DEFAULT (now())
 );
 
 CREATE TABLE "profile" (
-  "user" varchar NOT NULL,
-  "post_count" integer NOT NULL DEFAULT 0,
-  "subscriber_count" integer NOT NULL DEFAULT 0,
-  "subscription_count" integer NOT NULL DEFAULT 0,
+  "user" integer,
+  "post_count" integer DEFAULT 0,
+  "subscriber_count" integer DEFAULT 0,
+  "subscription_count" integer DEFAULT 0,
   "bio" varchar(1000)
 );
 
-COMMENT ON COLUMN "post"."content" IS 'Content of the post';
+CREATE TABLE "post" (
+  "slug" varchar(100) PRIMARY KEY,
+  "title" varchar(100) UNIQUE,
+  "body" text,
+  "author" integer,
+  "created_at" timestamptz DEFAULT (now())
+);
 
 COMMENT ON COLUMN "profile"."post_count" IS 'positive';
 
@@ -36,27 +33,12 @@ COMMENT ON COLUMN "profile"."subscriber_count" IS 'positive';
 
 COMMENT ON COLUMN "profile"."subscription_count" IS 'positive';
 
-ALTER TABLE "post" ADD FOREIGN KEY ("author") REFERENCES "user" ("id") ON DELETE SET NULL;
+COMMENT ON COLUMN "post"."body" IS 'Content of the post';
+
+ALTER TABLE "post" ADD FOREIGN KEY ("author") REFERENCES "user" ("id");
+
+ALTER TABLE "subscription" ADD FOREIGN KEY ("subscriber") REFERENCES "user" ("id");
+
+ALTER TABLE "subscription" ADD FOREIGN KEY ("target") REFERENCES "user" ("id");
 
 ALTER TABLE "profile" ADD FOREIGN KEY ("user") REFERENCES "user" ("id");
-
-CREATE TABLE "user_subscription" (
-  "user_id" integer,
-  "subscription_subscriber" integer,
-  PRIMARY KEY ("user_id", "subscription_subscriber")
-);
-
-ALTER TABLE "user_subscription" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
-
-ALTER TABLE "user_subscription" ADD FOREIGN KEY ("subscription_subscriber") REFERENCES "subscription" ("subscriber");
-
-
-CREATE TABLE "user_subscription(1)" (
-  "user_id" integer,
-  "subscription_traget" integer,
-  PRIMARY KEY ("user_id", "subscription_traget")
-);
-
-ALTER TABLE "user_subscription(1)" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
-
-ALTER TABLE "user_subscription(1)" ADD FOREIGN KEY ("subscription_traget") REFERENCES "subscription" ("traget");
